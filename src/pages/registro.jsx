b16348b7e52f4b1d4b5d57ed6ca447ddaff0b5c0
tech01-rapimoney <156877411+tech01-rapimoney@users.noreply.github.com>
2024-05-28 21:02:18 -0500

    actualizacion responsive

--- src/pages/registro.jsx
+++ src/pages/registro.jsx
@@ -50,7 +50,7 @@ const Registro = () => {
   };
 
   return (
-    <div className='flex flex-col gap-10 p-10 bg-sky-900 min-h-screen'>
+    <div className='flex flex-col gap-10 p-5 md:p-10 bg-sky-900 min-h-screen'>
       <h1 className='text-2xl font-bold text-white text-center'>Citas Medicas</h1>
       <div>
         <form onSubmit={handleSubmit} className="max-w-md mx-auto bg-">
@@ -111,36 +111,38 @@ const Registro = () => {
       </div>
       <div>
         <h2 className="text-xl text-white">Historial de Citas</h2>
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">N°</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">DNI</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Género</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fecha</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Especialidad</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Doctor</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Razón</th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {patients.map((patient, index) => (
-              patient.treatmentHistory.map((history, historyIndex) => (
-                <tr key={`${index}-${historyIndex}`}>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{index + 1}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{patient.dni}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{patient.name}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{patient.gender}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{history.date}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{history.specialty}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{history.doctor}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{history.reason}</td>
-                </tr>
-              ))
-            ))}
-          </tbody>
-        </table>
+        <div className=" overflow-x-auto">
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">N°</th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">DNI</th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Género</th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Fecha</th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Especialidad</th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Doctor</th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Razón</th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {patients.map((patient, index) => (
+                patient.treatmentHistory.map((history, historyIndex) => (
+                  <tr key={`${index}-${historyIndex}`}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{index + 1}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{patient.dni}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{patient.name}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{patient.gender}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{history.date}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{history.specialty}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{history.doctor}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{history.reason}</td>
+                  </tr>
+                ))
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
     
